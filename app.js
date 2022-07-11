5052bbdfe48c653cea1a418aab763c3977b4eb80
Alfiia Sadykova <alekakalm@123gmail.com>
2022-07-11 16:24:12 +0200

    MVP

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
-    const addTask = document.querySelector('.add>button')
+    const tasks = []
+    const addTask = document.querySelector('.input-group>.btn')
     const list = document.querySelector('.list')
+    const taskCount = document.querySelector('.tasks')
+    const taskText = document.querySelector('.input-group>.form-control')
+
 
     addTask.addEventListener('click', () => {
-        list.innerHTML += '  <div class="input-group mb-3">\n' +
-            '                <div class="input-group-text">\n' +
-            '                    <input class="form-check-input mt-0" type="checkbox" value="" >\n' +
-            '                </div>\n' +
-            '                <input type="text" class="form-control">\n' +
-            '            </div>'
-    })
 
+
+        list.innerHTML = '';
+
+        tasks.push({ title: taskText.value, state: ''})
+
+        tasks.forEach((task) => {
+            list.innerHTML += '<div class="input-group mb-3">\n' +
+                '                <div class="input-group-text">\n' +
+                '                    <input class="form-check-input mt-0" type="checkbox" value="" ' + task.state + ' >\n' +
+                '                </div>\n' +
+                '                <input type="text" class="form-control" value="'+ task.title + '" >\n' +
+                '            </div>'
+        })
+
+        taskText.value = ''
+
+        taskCount.innerHTML = tasks.length + 'task';
+
+
+       list.querySelectorAll('.form-check-input').forEach((checkbox, i) => {
+           checkbox.addEventListener('click', ()=>{
+               const task = tasks[i]
+               if (checkbox.checked) {
+                   task.state = 'checked'
+               }
+               else{
+                   task.state = ''
+               }
+               console.log(checkbox.checked)
+           })
+       })
+    })
 
 })
